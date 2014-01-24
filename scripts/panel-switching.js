7e4e53bc7d253bc000501045c80a19e07708e247
qqliu <quanquan.liu@mit.edu>
2014-01-24 14:59:47 -0500

    change javascript to jquery

--- scripts/panel-switching.js
+++ scripts/panel-switching.js
@@ -1,21 +1,21 @@
 /* Formerly part of browse.js ; look there for svn history */
 
 function onShowScheduleDetails() {
-    document.getElementById("browsing-interface").style.visibility = "hidden";
-    document.getElementById("schedule-preview-pane").style.visibility = "hidden";
-    document.getElementById("left-wrapper").style.visibility = "hidden";
+    $("#browsing-interface").css("visibility: hidden");
+    $("#schedule-preview-pane").css("visibility: hidden");
+    $("#left-wrapper").css("visibility: hidden");
     
-    document.getElementById("schedule-details-layer").style.visibility = "visible";
+    $("#schedule-details-layer").css("visibility: visible");
     
     scroll(0, 0);
 }
 
 function onShowSchedulePreview() {
-    document.getElementById("schedule-details-layer").style.visibility = "hidden";
+    $("#schedule-details-layer").css("visibility: hidden");
     
-    document.getElementById("browsing-interface").style.visibility = "visible";
-    document.getElementById("schedule-preview-pane").style.visibility = "visible";
-    document.getElementById("left-wrapper").style.visibility = "visible";
+    $("#browsing-interface").css("visibility: visible");
+    $("#schedule-preview-pane").css("visibility: visible");
+    $("left-wrapper").css("visibility: visible");
     
     scroll(0, 0);
 }
