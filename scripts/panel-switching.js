43a95e4f80f78ed44b9eae51d91e83bfa4166203
qqliu <quanquan.liu@mit.edu>
2014-01-24 15:04:10 -0500

    used jquery wrong doh

--- scripts/panel-switching.js
+++ scripts/panel-switching.js
@@ -1,21 +1,21 @@
 /* Formerly part of browse.js ; look there for svn history */
 
 function onShowScheduleDetails() {
-    $("#browsing-interface").css("visibility: hidden");
-    $("#schedule-preview-pane").css("visibility: hidden");
-    $("#left-wrapper").css("visibility: hidden");
+    $("#browsing-interface").css("visibility", "hidden");
+    $("#schedule-preview-pane").css("visibility", "hidden");
+    $("#left-wrapper").css("visibility", "hidden");
     
-    $("#schedule-details-layer").css("visibility: visible");
+    $("#schedule-details-layer").css("visibility", "visible");
     
     scroll(0, 0);
 }
 
 function onShowSchedulePreview() {
-    $("#schedule-details-layer").css("visibility: hidden");
+    $("#schedule-details-layer").css("visibility", "hidden");
     
-    $("#browsing-interface").css("visibility: visible");
-    $("#schedule-preview-pane").css("visibility: visible");
-    $("left-wrapper").css("visibility: visible");
+    $("#browsing-interface").css("visibility", "visible");
+    $("#schedule-preview-pane").css("visibility", "visible");
+    $("#left-wrapper").css("visibility", "visible");
     
     scroll(0, 0);
 }
