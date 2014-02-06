7015d8fad47ccf46fec0e870688545c5ab96d2bc
qqliu <qliu2011@gmail.com>
2014-02-05 19:05:26 -0500

    small cleanup

--- scripts/class-data-importer.js
+++ scripts/class-data-importer.js
@@ -12,43 +12,39 @@ registers json importer for processing class data
 */
 ExhibitImporter._register = function(evt, reg) {
     if (!reg.isRegistered(
-    Exhibit.Importer.JSONP._registryKey,
-    ExhibitImporter._type)) {
-        reg.register(
-            Exhibit.Importer.JSONP._registryKey,
-            ExhibitImporter._type,
-            ExhibitImporter
-        );
-	}
+        Exhibit.Importer.JSONP._registryKey,
+        ExhibitImporter._type)) {
+            reg.register(
+                Exhibit.Importer.JSONP._registryKey,
+                ExhibitImporter._type,
+                ExhibitImporter
+            );
+       }
 };
-
 /*
 @json input json for course data from data warehouse
 @return processed json for class data
 */
 ExhibitImporter.transformJSON = function(json, url, link) {
-	var crossListedClasses = [];
 	var items = json.items;
 
     for (var i = 0; i < items.length; i++) {
         var item = items[i];
 
         // If true this is a cross-listed class, and this is not the "master" class
-        // Ex. if this class is 18.062, rename it 6.042 and load 6.042
+        // Ex. if this class is 18.062, rename it 6.042
         if ('master_subject_id' in item && item.id != item.master_subject_id) {
             // Once both 18.062 and 6.042 are loaded, they will be merged as one class with two courses ["6", "18"]
             items[i] = {"id":item.master_subject_id, "course":item.course, "label":item.label, "type":item.type};
         }
         // Otherwise, this is a regular class -- just load it
         else {
+            //in load and process
             processOfficialDataItem(item);
         }
     }
 
-    if (crossListedClasses.length > 0) {
-        loadIndividualClasses(crossListedClasses);
-    }
-
+    //in load and process
     loadStaticData("data/user.php", window.database, setupLogin);
 
 	return json;
