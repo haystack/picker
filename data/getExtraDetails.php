4463f29d22328ed041524f6695cd9b7ca9866d6b
qqliu <quanquan.liu@mit.edu>
2014-08-28 11:13:02 -0400

    updated post.php to mysqli

--- data/getExtraDetails.php
+++ data/getExtraDetails.php
@@ -28,17 +28,17 @@ if(isset($_POST['getEnrollment'])) {
 			if ($rowuser = mysql_fetch_row($query)) {
 			    $items[] = '"' . $rowuser[0] . '"';
 			}
-		}	
+		}
 	} else {
 		$result = mysql_query("SELECT r_userid
 			      FROM attendance WHERE semester='" . $semester . "' AND r_classid='" . $classid . "';");
-		
+
 		while($row = mysql_fetch_row($result)) {
 			$query = mysql_query("SELECT u_athena FROM users WHERE u_userid = " . $row[0]);
 			if ($rowuser = mysql_fetch_row($query)) {
 			    $items[] = '"' . $rowuser[0] . '"';
 			}
-		}	
+		}
 	}
 }
 
@@ -65,39 +65,39 @@ if (isset($_POST['getComments'])) {
             } else {
                 $string .= ',"author":"' . $row[0] . '"';
             }
-            
+
             if ($row[6]) {
                 $string .= ', "negative" : "true"';
             }
-    
+
             if ($row[0] == $athena) {
-                $string .= ',"is-current-user":"true"';	
+                $string .= ',"is-current-user":"true"';
             }
-            
-            $query1 = mysql_query("SELECT * FROM votes 
-		WHERE vote = 1 AND userid = $userid AND commentid = '$row[5]'"); 
+
+            $query1 = mysql_query("SELECT * FROM votes
+		WHERE vote = 1 AND userid = $userid AND commentid = '$row[5]'");
             if ($vup = mysql_fetch_row($query1)) {
                 $string .= ', "votedUp": "true"';
             } else {
                 $string .= ', "votedUp": "false"';
             }
-    
-            $query2 = mysql_query("SELECT commentid FROM votes 
+
+            $query2 = mysql_query("SELECT commentid FROM votes
                     WHERE vote = 2 AND userid = $userid AND commentid = '$row[5]'");
             if ($vdown = mysql_fetch_row($query2)) {
                 $string .= ', "votedDown": "true"';
             } else {
                 $string .= ', "votedDown": "false"';
             }
-    
-            $query3 = mysql_query("SELECT commentid FROM votes 
+
+            $query3 = mysql_query("SELECT commentid FROM votes
                     WHERE flag = 1 AND userid = $userid AND commentid = '$row[5]'");
             if ($vflag = mysql_fetch_row($query3)) {
                 $string .= ', "flagClicked": "true"';
             } else {
                 $string .= ', "flagClicked": "false"';
             }
-            
+
             $string .= '}';
             $items[] = $string;
     }
