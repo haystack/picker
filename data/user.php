262705c2c3fe150da43e58fc5e0b6bda3e1c852e
Brandon Carter <bcarter@mit.edu>
2015-11-17 20:15:46 -0500

    better error catching for db login

--- data/user.php
+++ data/user.php
@@ -8,7 +8,7 @@ ini_set('display_errors', 'On');
 ini_set('allow_url_fopen', 'true');
 
 // Include the MySQL connect details
-include '../db_credentials.php';
+require('../db_credentials.php');
 
 $link = mysqli_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_NAME)
 	or die('MySQL connect failed');
